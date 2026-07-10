66afdbd74e3f714d4991641c199d294c395788f0
dannycode7 <danahosakadi1@icloud.com>
2026-07-10 17:00:52 +0200

    add cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // À garder si tu envoies des cookies ou des headers d'authentification (comme des sessions)
+  });
   app.useGlobalPipes(
   new ValidationPipe({
     whitelist: true,
@@ -11,6 +16,8 @@ async function bootstrap() {
     transform: true,
   }),
 );
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
